80705e89d38359e36f4a52a95a4579de42caa9d3
Paulo Felipe <paulofeliperezende@outlook.com>
2025-01-28 21:17:40 -0300

    Initial commit

---
+++ src/main/java/com/example/roads/RoadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.roads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roads/RoadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
