d4ac2d63965dafff7f53fd926afc4e3f8ac1cabb
Paulo Felipe <paulofeliperezende@outlook.com>
2025-01-28 21:17:40 -0300

    Atualizacao 28/01

---
+++ src/main/java/com/example/roads/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.roads.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* Aqui ficara todos os endpoints da aplicação */
+    @RestController()
+    public class HomeController {
+        @GetMapping("sayHello")
+        public String sayHello(){
+            return "Hello World";
+        }
+    }
