adbac4a0f3e7c0646a5516f6ae988c7c439da21b
Paulo Felipe <paulofeliperezende@outlook.com>
2025-02-07 13:03:58 -0300

    Atualizacao 03/02: Atualização em Rododvia

--- src/main/java/com/example/roads/entities/Rodovia.java
+++ src/main/java/com/example/roads/entities/Rodovia.java
@@ -10,15 +10,6 @@ import jakarta.persistence.*;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idRodovia")
-    private Long idRodovia;
-
-    @Column(name ="codigo")
-    private String codigo;;
-
-    @Column(name= "numSeguimento")
-    private int numSeguimento;
-
     @Column(name= "tipoRodovia")
     private String tipoRodovia;
 
@@ -29,31 +20,6 @@ import jakarta.persistence.*;
     public Rodovia() {
 
     }
-
-    public Long getIdRodovia() {
-        return idRodovia;
-    }
-
-    public void setIdRodovia(Long idRodovia) {
-        this.idRodovia = idRodovia;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public int getNumSeguimento() {
-        return numSeguimento;
-    }
-
-    public void setNumSeguimento(int numSeguimento) {
-        this.numSeguimento = numSeguimento;
-    }
-
     public String getTipoRodovia() {
         return tipoRodovia;
     }
