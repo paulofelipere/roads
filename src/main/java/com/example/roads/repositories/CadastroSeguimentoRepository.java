2e1961470e06ad7e2fe9fe4d01facff4a2323c30
Paulo Felipe <paulofeliperezende@outlook.com>
2025-03-01 23:49:25 -0300

    Atualizacao 01/03: Inicio do desenvolvimento da função de cadastro de seguimento

--- src/main/java/com/example/roads/repositories/CadastroSeguimentoRepository.java
+++ src/main/java/com/example/roads/repositories/CadastroSeguimentoRepository.java
@@ -1,8 +1,10 @@
 package com.example.roads.repositories;
 
 
+import com.example.roads.entities.CadastroSeguimento;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CadastroSeguimentoRepository  {
+public interface CadastroSeguimentoRepository extends JpaRepository<CadastroSeguimento, Long> {
 }
