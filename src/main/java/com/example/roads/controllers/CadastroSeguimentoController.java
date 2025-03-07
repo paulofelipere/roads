f256b39ca1f56cca860ca09b726200d0854356be
paulofelipere <paulofeliperezende@outlook.com>
2025-03-06 21:33:50 -0300

    Atualização 06/03

--- src/main/java/com/example/roads/controllers/CadastroSeguimentoController.java
+++ src/main/java/com/example/roads/controllers/CadastroSeguimentoController.java
@@ -1,4 +1,15 @@
 package com.example.roads.controllers;
 
+import com.example.roads.entities.CadastroSeguimento;
+import com.example.roads.entities.Rodovia;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CadastroSeguimentoController {
+
+    @GetMapping]
+    public List<CadastroSeguimento> getRodovia() {}
 }
