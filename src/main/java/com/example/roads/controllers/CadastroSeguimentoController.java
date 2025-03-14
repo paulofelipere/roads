d89f1c24efaa01e2845ae11c57c5e89d2d6d63b0
Paulo Felipe <paulofeliperezende@outlook.com>
2025-03-13 22:47:58 -0300

    Atualização 13/03: Construção do código em CadastroSeguimento, CadastroSeguimentoController e CadastroSeguimentoService

--- src/main/java/com/example/roads/controllers/CadastroSeguimentoController.java
+++ src/main/java/com/example/roads/controllers/CadastroSeguimentoController.java
@@ -2,7 +2,11 @@ package com.example.roads.controllers;
 
 import com.example.roads.entities.CadastroSeguimento;
 import com.example.roads.entities.Rodovia;
+import com.example.roads.services.CadastroSeguimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,6 +14,31 @@ import java.util.List;
 @RestController
 public class CadastroSeguimentoController {
 
-    @GetMapping]
-    public List<CadastroSeguimento> getRodovia() {}
+    @Autowired
+    private CadastroSeguimentoService cadastroSeguimentoService;
+
+    @GetMapping("getCadastroSeguimento")
+    public List<CadastroSeguimento> getCadastroSeguimento() {
+        return cadastroSeguimentoService.getCadastroSeguimento();
+    }
+
+    @PostMapping("addCadastroSeguimento")
+    public CadastroSeguimento saveCadastroSeguimento(@RequestBody CadastroSeguimento cadastroSeguimento) {
+        return cadastroSeguimentoService.saveCadastroSeguimento(cadastroSeguimento);
+    }
+
+    @PostMapping("deleteCadastroSeguimento")
+    public CadastroSeguimento deleteCadastroSeguimento(@RequestBody CadastroSeguimento cadastroSeguimento) {
+        return cadastroSeguimentoService.deleteCadastroSeguimento(cadastroSeguimento);
+    }
+
+    @PostMapping("findCadastroSeguimentoById")
+    public CadastroSeguimento findCadastroSeguimentoById(@RequestBody Long id) {
+        return cadastroSeguimentoService.findCadastroSeguimentoById(id);
+    }
+
+    @PostMapping("updateCadastroSeguimento")
+    public CadastroSeguimento updateCadastroSeguimento(@RequestBody CadastroSeguimento cadastroSeguimento) {
+        return cadastroSeguimentoService.updateCadastroSeguimento(cadastroSeguimento);
+    }
 }
