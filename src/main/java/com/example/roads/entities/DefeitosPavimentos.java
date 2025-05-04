0345be92ef2530354886d8353d0b188542f8cede
Paulo Felipe <paulofeliperezende@outlook.com>
2025-05-04 19:24:47 -0300

    Atualização 04/05: Mudanças em DefeitosPavimentos

--- src/main/java/com/example/roads/entities/DefeitosPavimentos.java
+++ src/main/java/com/example/roads/entities/DefeitosPavimentos.java
@@ -1,6 +1,7 @@
 package com.example.roads.entities;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "defeitosPavimentos")
@@ -9,20 +10,22 @@ public class DefeitosPavimentos {
     @Id
     @Column(name = "codigo")
     private String codigo;
-// add chaves externas
+
     @ManyToMany
-    @JoinColumn(name = "cadastroSeguimento")
-    private CadastroSeguimento cadastroSeguimento;
+    @JoinTable(
+            name = "defeitosseguimentos",
+            joinColumns = @JoinColumn(name = "codigo"),
+            inverseJoinColumns = @JoinColumn(name = "numSeguimento")
+    )
+    private List<CadastroSeguimento> cadastroSeguimento;
 
     @Column(name = "tipodefeito")
     private String tipoDefeito;
 
-    @Column(name ="numseguimento")
+    @Column(name = "numseguimento")
     private String numSeguimento;
 
-
-    public DefeitosPavimentos(){
-
+    public DefeitosPavimentos() {
     }
 
     public String getCodigo() {
@@ -31,5 +34,29 @@ public class DefeitosPavimentos {
 
     public void setCodigo(String codigo) {
         this.codigo = codigo;
+    }
+
+    public List<CadastroSeguimento> getCadastroSeguimento() {
+        return cadastroSeguimento;
+    }
+
+    public void setCadastroSeguimento(List<CadastroSeguimento> cadastroSeguimento) {
+        this.cadastroSeguimento = cadastroSeguimento;
+    }
+
+    public String getTipoDefeito() {
+        return tipoDefeito;
+    }
+
+    public void setTipoDefeito(String tipoDefeito) {
+        this.tipoDefeito = tipoDefeito;
+    }
+
+    public String getNumSeguimento() {
+        return numSeguimento;
+    }
+
+    public void setNumSeguimento(String numSeguimento) {
+        this.numSeguimento = numSeguimento;
     }
 }
