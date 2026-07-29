a746b8fc5c7a6d27255b80c2013ad705946e151d
Paulo Felipe <paulofeliperezende@outlook.com>
2026-07-28 23:34:14 -0300

    Atualização (28/07/2026)

--- src/main/java/com/example/roads/entities/Escorregamento.java
+++ src/main/java/com/example/roads/entities/Escorregamento.java
@@ -1,4 +1,77 @@
 package com.example.roads.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Escorregamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "codigoEscorregamento")
+    private String codigoEscorregamento;
+
+    @Column(name = "porcetagemEscorregamentoKm")
+    private double porcetagemEscorregamentoKm;
+
+    @Column(name = "gravidadeEscorregamento")
+    private int gravidadeEscorregamento;
+
+    @Column(name = "foap")
+    private double foap;
+
+    @Column(name = "poap")
+    private double poap;
+
+    public Escorregamento() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigoEscorregamento() {
+        return codigoEscorregamento;
+    }
+
+    public void setCodigoEscorregamento(String codigoEscorregamento) {
+        this.codigoEscorregamento = codigoEscorregamento;
+    }
+
+    public int getGravidadeEscorregamento() {
+        return gravidadeEscorregamento;
+    }
+
+    public void setGravidadeEscorregamento(int gravidadeEscorregamento) {
+        this.gravidadeEscorregamento = gravidadeEscorregamento;
+    }
+
+    public double getPorcetagemEscorregamentoKm() {
+        return porcetagemEscorregamentoKm;
+    }
+
+    public void setPorcetagemEscorregamentoKm(double porcetagemEscorregamentoKm) {
+        this.porcetagemEscorregamentoKm = porcetagemEscorregamentoKm;
+    }
+
+    public double getFoap() {
+        return foap;
+    }
+
+    public void setFoap(double foap) {
+        this.foap = foap;
+    }
+
+    public double getPoap() {
+        return poap;
+    }
+
+    public void setPoap(double poap) {
+        this.poap = poap;
+    }
 }
