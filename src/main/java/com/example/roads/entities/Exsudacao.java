a85897b75f6215410983cb72bc3f9154fc2f5cf1
Paulo Felipe <paulofeliperezende@outlook.com>
2026-08-02 03:21:15 -0300

    Adição das classes Exsudacao.java, ExsudacaoRepository.java e ExsudacaoService.java (02/08/2026)

--- src/main/java/com/example/roads/entities/Exsudacao.java
+++ src/main/java/com/example/roads/entities/Exsudacao.java
@@ -1,4 +1,80 @@
 package com.example.roads.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Exsudacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "codigoExsudacao")
+    private String codigoExsudacao;
+
+    @Column(name = "porcetagemExsudacaoKm")
+    private double porcetagemExsudacaoKm;
+
+    @Column(name = "gravidadeExsudacao")
+    private int gravidadeExsudacao;
+
+    @Column(name = "foap")
+    private double foap;
+
+    @Column(name = "poap")
+    private double poap;
+
+    public Exsudacao() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigoExsudacao() {
+        return codigoExsudacao;
+    }
+
+    public void setCodigoExsudacao(String codigoExsudacao) {
+        this.codigoExsudacao = codigoExsudacao;
+    }
+
+    public double getPorcetagemExsudacaoKm() {
+        return porcetagemExsudacaoKm;
+    }
+
+    public void setProcetagemExsudacaoKm(double procetagemExsudacaoKm) {
+        this.porcetagemExsudacaoKm = procetagemExsudacaoKm;
+    }
+
+    public double getFoap() {
+        return foap;
+    }
+
+    public void setFoap(double foap) {
+        this.foap = foap;
+    }
+
+    public int getGravidadeExsudacao() {
+        return gravidadeExsudacao;
+    }
+
+    public void setGravidadeExsudacao(int gravidadeExsudacao) {
+        this.gravidadeExsudacao = gravidadeExsudacao;
+    }
+
+    public void setPorcetagemExsudacaoKm(double porcetagemExsudacaoKm) {
+        this.porcetagemExsudacaoKm = porcetagemExsudacaoKm;
+    }
+
+    public double getPoap() {
+        return poap;
+    }
+
+    public void setPoap(double poap) {
+        this.poap = poap;
+    }
 }
---
+++ src/main/java/com/example/roads/services/ExsudacaoService.java
@@ -0,0 +1,76 @@
+package com.example.roads.services;
+
+import com.example.roads.entities.Exsudacao;
+import com.example.roads.entities.Ondulacoes;
+import com.example.roads.repositories.ExsudacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExsudacaoService {
+    private static final double LIMIAR_ALTA = 0.50;
+    private static final double LIMIAR_MEDIA = 0.10;
+    private static final double FATOR_ALTO = 0.65;
+    private static final double FATOR_MEDIO = 0.45;
+    private static final double FATOR_BAIXO = 0.30;
+
+    @Autowired
+    private ExsudacaoRepository exsudacaoRepository;
+
+    public ExsudacaoService(){
+
+    }
+
+    public List<Exsudacao> getExsudacoes(){return exsudacaoRepository.findAll();}
+
+    public Exsudacao saveExsudacao(Exsudacao exsudacao){
+        calculoExsudacao(exsudacao);
+        return exsudacaoRepository.save(exsudacao);
+    }
+    public Exsudacao findExsudacaoById(long id){
+        return exsudacaoRepository.findById(id).get();
+    }
+    public Exsudacao updateExsudacao(Exsudacao exsudacao){
+        calculoExsudacao(exsudacao);
+        return exsudacaoRepository.save(exsudacao);
+    }
+
+    public void calculoExsudacao(Exsudacao exsudacao){
+        double porcetagemExsudacaoKm = exsudacao.getPorcetagemExsudacaoKm();
+
+        if (porcetagemExsudacaoKm >= LIMIAR_ALTA){
+            exsudacao.setCodigoExsudacao("A");
+            exsudacao.setGravidadeExsudacao(3);
+        }else if(porcetagemExsudacaoKm >= LIMIAR_MEDIA){
+            exsudacao.setCodigoExsudacao("B");
+            exsudacao.setGravidadeExsudacao(2);
+        }else {
+            exsudacao.setCodigoExsudacao("C");
+            exsudacao.setGravidadeExsudacao(1);
+        }
+    }
+
+    public void pesoFoap(Exsudacao exsudacao){
+        String codigo = exsudacao.getCodigoExsudacao();
+        if ("A".equals(codigo)) {
+            exsudacao.setFoap(FATOR_ALTO);
+        } else if ("M".equals(codigo)) {
+            exsudacao.setFoap(FATOR_MEDIO);
+        } else {
+            exsudacao.setFoap(FATOR_BAIXO);
+        }
+    }
+
+    private void calculoPoap(Exsudacao exsudacao) {
+        int gravidade = exsudacao.getGravidadeExsudacao();
+        if (gravidade == 3) {
+            exsudacao.setPoap(FATOR_ALTO);
+        } else if (gravidade == 2) {
+            exsudacao.setPoap(FATOR_MEDIO);
+        } else {
+            exsudacao.setPoap(FATOR_BAIXO);
+        }
+    }
+}
